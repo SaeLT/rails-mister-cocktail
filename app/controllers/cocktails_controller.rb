b5950e7ed9a3515671bed2b5c8ce5e23947d7e8a
Sophie Labouysse <sophie.labouysse@gmail.com>
2020-08-13 16:34:13 +0200

    cocktails controller done

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,23 @@
+class CocktailsController < ApplicationController
+    def index
+        @cocktails = Cocktail.all
+    end
+
+    def new
+        @cocktails = Cocktail.new
+    end
+
+    def create
+        @cocktail = Cocktail.new(cocktail_params)
+
+        if @cocktail.save
+        redirect_to @cocktail, notice: 'A Cocktail was successfully created.'
+        else
+        render :new
+        end
+    end
+
+    def show
+        @cocktail = Cocktail.find(params[:id])
+    end
+end
